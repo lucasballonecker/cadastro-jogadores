85e611f502c389b50e898a7817b65d29be91ece9
Lucas Ballonecker da Costa <lucasballoneckercosta@gmail.com>
2025-05-26 12:43:19 -0300

    Primeiro commit

---
+++ src/main/java/com/lucasballonecker/cadastro_jogadores/CadastroJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.lucasballonecker.cadastro_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasballonecker/cadastro_jogadores/CadastroJogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasballonecker.cadastro_jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
