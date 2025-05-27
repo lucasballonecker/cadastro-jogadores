3903a2fae8b157bcf5f4cb0bc5a5f06d1baf43af
Lucas Ballonecker da Costa <lucasballoneckercosta@gmail.com>
2025-05-27 13:48:55 -0300

    Adicionando busca de codinomes do grupo 'VINGADORES'

---
+++ src/main/java/com/lucasballonecker/cadastro_jogadores/web/VingadoresDTO.java
@@ -0,0 +1,13 @@
+package com.lucasballonecker.cadastro_jogadores.web;
+
+import java.util.List;
+
+public record VingadoresDTO(List<Codinome> vingadores) implements  CodinomeDTO{
+
+    @Override
+    public List<String> getCodinomes() {
+        return vingadores.stream()
+                .map(Codinome::codinome).toList();
+    }
+}
+record Codinome(String codinome){}
